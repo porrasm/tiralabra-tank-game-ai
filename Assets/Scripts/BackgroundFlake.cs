0859cf011ac156d9c65d0d598b73354a0b14469a
porrasm <31691452+porrasm@users.noreply.github.com>
2019-05-04 20:48:33 +0300

    Background flaker test

---
+++ Assets/Scripts/BackgroundFlake.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFlake : MonoBehaviour {
+
+	private float boundX = 960 + 100;
+	private float boundY = 540 + 100;
+
+	private Vector3 direction;
+	private float velocity;
+	private float rotation;
+
+	private RectTransform rect;
+
+	// Use this for initialization
+	void Start () {
+
+		rect = GetComponent<RectTransform>();	
+
+		Randomize();
+	}
+	
+	private void Randomize() {
+
+		velocity = Random.value * 50 + 25;
+		rotation = Random.value * 100 - 50;
+		if (rotation >= 0) {
+			rotation += 25;
+		} else {
+			rotation -= 25;
+		}
+		direction = new Vector3(Random.value * 2 - 1, -Random.value).normalized;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		CheckPosition();
+		UpdatePosition();
+	}
+
+	private void UpdatePosition() {
+		rect.position += direction * velocity * Time.deltaTime;
+		rect.Rotate(transform.forward * rotation * Time.deltaTime);
+	}
+	private void CheckPosition() {
+		if (Mathf.Abs(rect.localPosition.x) >= boundX) {
+			Destroy(gameObject);
+		} else if (Mathf.Abs(rect.localPosition.y) >= boundY) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/BackgroundFlaker.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFlaker : MonoBehaviour {
+
+	[SerializeField]
+	private float time;
+	private float timePassed;
+
+	private float width;
+	private float height;
+	
+
+	private GameObject flakePrefab;
+
+	// Use this for initialization
+	void Start () {
+
+		Vector3[] corners = new Vector3[4];
+		GetComponent<RectTransform>().GetLocalCorners(corners);
+
+		width = Vector3.Distance(corners[0], corners[3]);
+		height = Vector3.Distance(corners[0], corners[1]);
+
+		print("W: " + width);
+		print("H: " + height);
+
+		if (time == 0) {
+			time = 1;
+		}
+		flakePrefab = Resources.Load<GameObject>("ResourcePrefabs/BackgroundFlake");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateTimes();
+	}
+
+	private void UpdateTimes() {
+		timePassed += Time.deltaTime;
+
+		while (timePassed >= time) {
+			timePassed -= time;
+			SpawnFlake();
+		}
+	}
+	private void SpawnFlake() {
+		GameObject newFlake = Instantiate(flakePrefab);
+		newFlake.GetComponent<RectTransform>().localPosition = new Vector3(Random.value * width, height + 50);
+		print(newFlake.GetComponent<RectTransform>().localPosition);
+		newFlake.GetComponent<RectTransform>().SetParent(transform);
+	}
+}
