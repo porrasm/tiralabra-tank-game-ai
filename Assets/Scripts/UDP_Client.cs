6c58f0f9563cf2cf5e1308ad4bd476035e2a4a30
porrasm <31691452+porrasm@users.noreply.github.com>
2019-05-14 19:10:27 +0300

    Add working UDP server with support for multiple clients

---
+++ Assets/Scripts/UDP_Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class UDP_Client : MonoBehaviour {
+
+    string IP = "127.0.0.1";
+
+    IPEndPoint remoteEndPoint;
+    UdpClient client;
+
+    public KeyCode messageKey;
+    public string message;
+
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.C)) {
+            Connect();
+        }
+        if (Input.GetKeyDown(messageKey)) {
+            Send(message);
+        }
+    }
+
+    public void Connect() {
+        // Endpunkt definieren, von dem die Nachrichten gesendet werden.
+        print("UDPSend.init()");
+
+
+        // ----------------------------
+        // Senden
+        // ----------------------------
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), UDP_Server.PORT);
+        client = new UdpClient();
+
+        // status
+        print("Sending to " + IP + " : " + UDP_Server.PORT);
+        print("Testing: nc -lu " + IP + " : " + UDP_Server.PORT);
+    }
+
+
+    private void Send(string message) {
+        try {
+
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            client.Send(data, data.Length, remoteEndPoint);
+        } catch (Exception err) {
+            print(err.ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/UDP_Server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class UDP_Server : MonoBehaviour {
+
+    private Thread receiveThread;
+    private UdpClient client;
+
+    private string latestPacket;
+    private List<string> packets;
+
+    public static int PORT = 5000;
+
+    void Start() {
+        InitializeServer();
+    }
+
+    public void InitializeServer() {
+
+        print("Initializing server");
+
+        receiveThread = new Thread(new ThreadStart(Receive));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+    }
+
+    private void Receive() {
+
+        client = new UdpClient(PORT);
+
+        while (true) {
+
+            try {
+                // Bytes empfangen.
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+
+                // Bytes mit der UTF8-Kodierung in das Textformat kodieren.
+                string text = Encoding.UTF8.GetString(data);
+
+                // Den abgerufenen Text anzeigen.
+                print("Message from client: " + text);
+
+                // latest UDPpacket
+                latestPacket = text;
+
+                // ....
+                packets.Add(latestPacket);
+
+            } catch (Exception err) {
+                print(err.ToString());
+            }
+        }
+    }
+}
