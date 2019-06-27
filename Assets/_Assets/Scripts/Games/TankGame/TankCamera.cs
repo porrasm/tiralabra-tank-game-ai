ab7527e5715abe99bd71078dbcf462df772d4a55
porrasm <31691452+porrasm@users.noreply.github.com>
2019-06-27 18:05:13 +0300

    Start to add perspective camera

--- Assets/_Assets/Scripts/Games/TankGame/TankCamera.cs
+++ Assets/_Assets/Scripts/Games/TankGame/TankCamera.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TankCamera : MonoBehaviour
-{
+public class TankCamera : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
---
+++ Assets/_Assets/Scripts/Games/TankGame/TankPerspectiveCameraHelper.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankPerspectiveCameraHelper : MonoBehaviour {
+
+    private Transform prsCamera;
+    private Transform prsArea;
+    private GameObject level;
+
+    void Start() {
+        prsCamera = transform.GetChild(0);
+        prsArea = new GameObject().transform;
+        prsArea.name = "Perspective Level";
+        level = GameObject.FindGameObjectWithTag("Level");
+
+        SetPerspectiveGameArea();
+    }
+
+    public void SetPerspectiveGameArea() {
+        Clean();
+        SetArea();
+        CopyLevel();
+        SetCamera();
+    }
+
+    private void Clean() {
+        foreach (Transform child in prsArea) {
+            Destroy(child.gameObject);
+        }
+    }
+
+    private void SetArea() {
+        prsArea.position = ToPrspPos(Vector3.zero);
+    }
+    private void CopyLevel() {
+
+        GameObject copyLevel = Instantiate(level);
+        copyLevel.transform.parent = prsArea;
+        copyLevel.transform.localPosition = Vector3.zero;
+
+        CleanLevel(copyLevel.transform);
+    }
+    private void CleanLevel(Transform level) {
+
+        Destroy(level.Find("Spawns").gameObject);
+
+    }
+    private void SetCamera() {
+        prsCamera.position = ToPrspPos(transform.position);
+    }
+
+    public static Vector3 ToPrspPos(Vector3 pos) {
+        pos.x += TankSettings.LevelWidth * 2;
+        return pos;
+    }
+    public static Vector3 ToNormalPos(Vector3 pos) {
+        pos.x -= TankSettings.LevelWidth * 2;
+        return pos;
+    }
+}
